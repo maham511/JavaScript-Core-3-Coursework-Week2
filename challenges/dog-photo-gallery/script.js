c37e45c4ef22c2c124402f65b3b8e2da8668e96e
maham511 <83180548+maham511@users.noreply.github.com>
2021-09-20 20:23:50 +0100

    Create script.js

---
+++ challenges/dog-photo-gallery/script.js
@@ -0,0 +1,22 @@
+//ul & btn elems stored
+let ul = document.getElementById('ul');
+let button = document.getElementById('btn');
+
+//eventlistener callback fetches api, then logs and creates li & img element
+function fetchDog(){
+fetch('https://dog.ceo/api/breeds/image/random')
+  .then(response => {
+    return response.json();
+  })
+  .then(greeting => {
+  console.log(greeting);
+  let li = document.createElement('li');
+  let img = document.createElement('img');
+  ul.appendChild(li);
+  li.append(img);
+  img.src= greeting.message;
+  });
+}
+
+//onclick button adds random dog img to page
+button.addEventListener('click', fetchDog);
