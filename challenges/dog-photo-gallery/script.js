302f7cb0693816be05cb28064a4d951822b4c2ba
maham511 <83180548+maham511@users.noreply.github.com>
2021-09-20 20:54:05 +0100

    Update script.js

--- challenges/dog-photo-gallery/script.js
+++ challenges/dog-photo-gallery/script.js
@@ -1,22 +1,23 @@
 //ul & btn elems stored
-let ul = document.getElementById('ul');
-let button = document.getElementById('btn');
+let ul = document.getElementById("ul");
+let button = document.getElementById("btn");
 
 //eventlistener callback fetches api, then logs and creates li & img element
-function fetchDog(){
-fetch('https://dog.ceo/api/breeds/image/random')
-  .then(response => {
-    return response.json();
-  })
-  .then(greeting => {
-  console.log(greeting);
-  let li = document.createElement('li');
-  let img = document.createElement('img');
-  ul.appendChild(li);
-  li.append(img);
-  img.src= greeting.message;
-  });
+function fetchDog() {
+  fetch("https://dog.ceo/api/breeds/image/random")
+    .then((response) => {
+      return response.json();
+    })
+    .then((greeting) => {
+      console.log(greeting);
+      let li = document.createElement("li");
+      let img = document.createElement("img");
+      ul.appendChild(li);
+      li.append(img);
+      img.src = greeting.message;
+      img.width = 200;
+    });
 }
 
 //onclick button adds random dog img to page
-button.addEventListener('click', fetchDog);
+button.addEventListener("click", fetchDog);
